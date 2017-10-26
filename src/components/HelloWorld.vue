a512e6ca7061f0e6d290d3fdaffa020c3ba8aca0
Joshua Morony <joshuamorony@Joshuas-MacBook-Pro.local>
2017-10-26 11:09:32 +1030

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <ion-app>
+    <ion-header>
+
+        <ion-navbar>
+            <ion-title>Ionic</ion-title>
+        </ion-navbar>
+
+    </ion-header>
+
+    <ion-content>
+
+      <ion-button @click="goToDetail">Hello</ion-button>
+
+    </ion-content>
+  </ion-app>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    goToDetail () {
+      this.$router.push('detail')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
